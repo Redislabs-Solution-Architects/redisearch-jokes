79a72ac92b7f12b9a5eea55ad64803baca1ca142
laurentdroin <laurent@redislabs.com>
2018-12-27 14:27:05 -0800

    Ready to go...

--- populate_db.py
+++ populate_db.py
@@ -1,2 +1,23 @@
+import json
+from redisearch import Client, TextField, TagField
+
+with open('wocka.json', 'r') as f:
+    jokes = json.load(f)
+
+hostname = 'redis-17235.laurent.cs.redislabs.com'
+port = 17235
 
 
+client = Client('jokes', hostname, port)
+client.create_index((TextField('title'), TextField('body'), TextField('category'), TagField('label')))
+
+for joke in jokes:
+    client.add_document(joke['id'], title=joke['title'], body=joke['body'], category=joke['category'],
+                        label=joke['category'])
+
+
+print("number of jokes in the json file: " + str(len(jokes)))
+
+
+info = client.info()
+print(info)
